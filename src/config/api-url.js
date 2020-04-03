875d9f256babf56fb866485b8cd41094ab07297c
yael.lotan <yaelotan19@gmail.com>
2020-04-03 19:25:55 +0300

    fix(config/api-url.js):changes the url to the aws server

--- src/config/api-url.js
+++ src/config/api-url.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === 'development' ?
     'http://localhost:5000' :
-    'http://partners.infinitylabs.co.il:5000';
+    'http://ec2-34-219-150-204.us-west-2.compute.amazonaws.com:5000/';
 
 export default API_URL;
